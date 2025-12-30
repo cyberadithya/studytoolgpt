0396cef92732bb279169d940460cfd88a6965877
cyberadithya <cyber.adithya@gmail.com>
2025-12-30 17:18:17 -0500

    Started React

---
+++ studytoolsgpt/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Sidebar">
+        <div className="upperSidebar">
+
+        </div>
+        <div className="lowerSidebar">
+
+        </div>
+      </div>
+      <div className="Main">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
